496b99faad7d5aee97088eef0e34218e5e31695f
Hayanson <answnstjr314@naver.com>
2022-11-02 17:21:28 +0900

    ch03 study

--- MoonJunSeok/회의실 배정.py
+++ MoonJunSeok/회의실 배정.py
@@ -1,19 +1,18 @@
 N = int(input())
 conlist = []
-availList = []
 for i in range(N):
     tmpList = list(map(int,input().split()))
     conlist.append(tmpList)
 
-print(conlist)
+conlist.sort(key = lambda x:x[0])
 conlist.sort(key = lambda x:x[1])
-print(conlist)
-availList.append(conlist[0])
 
-for con in conlist:
-    if(availList[-1][1]<=con[0]):
-        availList.append(con)
+ctr = 0
+end_time = 0
+for st, ed in conlist:
+    if(st>=end_time):
+        end_time = ed
+        ctr+=1
         
         
-print(len(availList))
-print(availList)
+print(ctr)
